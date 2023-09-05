e299fc4e2e00bd0c1d48b72c6d6b7bb01007096b
Ivan <serralta.ivan@gmail.com>
2023-09-05 18:43:12 -0300

    nada

--- Probando.js
+++ Probando.js
@@ -8,3 +8,17 @@ for (let i = 2; i < num;i++){
 
 
 }
+
+function DecimalABinario(num) {
+    let enBin = 0;
+    let i= 0;
+    
+    do{
+        num%2==1?enBin+=Math.pow(10,i):null;
+        num=Math.trunc(num/2);
+        i++;
+    }while(num/2!=0 );
+    
+    return enBin
+ }
+ console.log(DecimalABinario(2));
